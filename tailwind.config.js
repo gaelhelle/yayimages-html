2bfaeb4b68730497e2845a0567416c74c243b39c
Gael (perso) <me@gaelhelle.fr>
2023-03-10 18:17:40 -0800

    add homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
           body1: "#666666",
           "xl-lightgray": "#F1F3F8",
           accent: "#3A4DF1",
+          accentDark: "#2B38AE",
         },
         black: "#111827",
       },
@@ -27,6 +28,13 @@ module.exports = {
       },
       boxShadow: {
         modal: "0 18px 30px 0 rgba(48, 69, 122, 0.31)",
+        carousel: "0px 6px 15px 0px rgba(128, 141, 165, 0.5)",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
+      transitionTimingFunction: {
+        "in-expo": "cubic-bezier(0.93, -0.04, 0.58, 1)",
       },
     },
   },
